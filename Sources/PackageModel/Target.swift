5bdbf694980050e21f05131f3ae1633aa74920ce
Nick Cooke <nickcooke@google.com>
2022-11-18 23:42:49 -0500

    Generate resource bundle accessors for Clang sources

--- Sources/PackageModel/Target.swift
+++ Sources/PackageModel/Target.swift
@@ -638,6 +638,7 @@ public final class MixedTarget: Target {
 
         self.clangTarget = try ClangTarget(
             name: name,
+            potentialBundleName: potentialBundleName,
             cLanguageStandard: cLanguageStandard,
             cxxLanguageStandard: cxxLanguageStandard,
             includeDir: includeDir,
@@ -645,7 +646,11 @@ public final class MixedTarget: Target {
             headers: headers,
             type: type,
             path: path,
-            sources: clangSources
+            sources: clangSources,
+            resources: resources,
+            ignored: ignored,
+            others: others,
+            buildSettings: buildSettings
         )
 
         super.init(
